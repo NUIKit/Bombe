beb7fb1e941c1fe340248c0d4e27022bbbcec3b4
Robert Widmann <devteam.codafi@gmail.com>
2015-04-15 16:29:26 -0600

    Consolidate the block header

--- src/BOMStore.c
+++ src/BOMStore.c
@@ -10,20 +10,19 @@
 #include "BOMStream.h"
 #include "Internal.h"
 
-typedef struct {
-	uint32_t unknown;
-	uint32_t indexOffset;
-	uint32_t indexLength;
-	uint32_t tocOffset;
-	uint32_t tocSize;
-} BOMHeader;
-
 struct __BOMStore {
-	BOMHeader header;
+	struct {
+		uint32_t blockCount;
+		uint32_t indexOffset;
+		uint32_t indexLength;
+		uint32_t tocOffset;
+		uint32_t tocSize;
+	} header;
+	
 	void *mmapAddr;
 	size_t mmapSize;
 
-	int32_t blockCount;
+	int32_t blockTableCount;
 	BOMBlock *blockTable;
 	ssize_t blockTableSize;
 };
@@ -56,6 +55,7 @@ BOMStoreRef BOMStoreCreateWithPath(const char *path) {
 
 	// All BOM files begin with `BOMStore`
 	if ((head1 != 'BOMS') || (head2 != 'tore')) {
+		close(fd);
 		BOMStreamFree(stream);
 		return NULL;
 	}
@@ -65,44 +65,44 @@ BOMStoreRef BOMStoreCreateWithPath(const char *path) {
 	
 	// We only read version 1.
 	if (version != 1) {
-//		fprintf(stderr, "%s has an unknown version: 0x%X\n", path, version);
+		fprintf(stderr, "BOM file at path '%s' has an unknown version: %d\n", path, version);
 	}
 	
 	struct __BOMStore *store = malloc(sizeof(struct __BOMStore));
 	if (store == NULL) {
+		close(fd);
 		BOMStreamFree(stream);
 		return NULL;
 	}
 	
 	// Read in the header.
-	BOMStreamReadUInt32(stream, &store->header.unknown);
+	BOMStreamReadUInt32(stream, &store->header.blockCount);
 	BOMStreamReadUInt32(stream, &store->header.indexOffset);
 	BOMStreamReadUInt32(stream, &store->header.indexLength);
 	BOMStreamReadUInt32(stream, &store->header.tocOffset);
 	BOMStreamReadUInt32(stream, &store->header.tocSize);
 	BOMStreamFree(stream);
 	
-	/// mmap in the BOM file.
+	// mmap in the BOM file.
 	if ((store->mmapAddr = mmap(NULL, stat.st_size, PROT_READ, MAP_ANON | MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
+		close(fd);
 		BOMStoreFree(store);
 		return NULL;
 	}
 	
+	// Create the block table...
 	BOMStoreCreateBlockTable(store);
+	
+	//  Stream the index into memory.
 	BOMStreamRef indexStream = BOMStreamCreateWithBuffer(store->mmapAddr + store->header.indexOffset, store->header.indexLength);
-	
-	if (indexStream == NULL) {
+	if (BOMReadBlockTable(store, indexStream) == false) {
+		close(fd);
 		BOMStoreFree(store);
 		return NULL;
 	}
 	
+	close(fd);
 	BOMStreamFree(indexStream);
-	
-	BOMStreamRef blockStream = BOMStreamCreateWithBuffer(store->mmapAddr + store->header.indexOffset, store->header.indexLength);
-	if (BOMReadBlockTable(store, blockStream) == false) {
-		BOMStoreFree(store);
-		return NULL;
-	}
 	
 	return store;
 }
@@ -171,34 +171,34 @@ static void BOMStoreCreateBlockTable(BOMStoreRef sto) {
 		if (size % vm_page_size != 0) {
 			size += vm_page_size - (size % vm_page_size);
 		}
-		if (vm_allocate(mach_task_self(), (vm_address_t *)&store->blockTable, size, 0x1) != KERN_SUCCESS) {
+		if (vm_allocate(mach_task_self(), (vm_address_t *)&store->blockTable, size, VM_FLAGS_ANYWHERE) != KERN_SUCCESS) {
 			store->blockTable = 0;
 		}
 		store->blockTableSize = size;
 	}
 }
 
-static bool BOMReadBlockTable(BOMStoreRef st, BOMStreamRef arg1) {
+static bool BOMReadBlockTable(BOMStoreRef st, BOMStreamRef stream) {
 	struct __BOMStore *store = (struct __BOMStore *)st;
 	uint32_t count;
-	BOMStreamReadUInt32(arg1, &count);
+	BOMStreamReadUInt32(stream, &count);
 	
-	if (count != 0x0) {
+	if (count != 0) {
 		if ((count > 0x1fffffff) || ((count << 0x3) > store->header.indexLength)) {
 			fwrite("bad value for block table count\n", 0x20, 0x1, stderr);
 			return false;
 		}
 		BOMExpandBlockTable(st, count * sizeof(BOMBlock));
 		if (store->blockTable != 0) {
-			store->blockCount = count;
+			store->blockTableCount = count;
 			for (size_t i = 0; i < count; i++) {
 				BOMBlock block;
 				uint32_t index, size;
-				BOMStreamReadUInt32(arg1, &index);
-				BOMStreamReadUInt32(arg1, &size);
+				BOMStreamReadUInt32(stream, &index);
+				BOMStreamReadUInt32(stream, &size);
 				block.index = index;
 				block.size = size;
-				BOMStreamAdvance(arg1, sizeof(BOMBlock) + block.size);
+				BOMStreamAdvance(stream, sizeof(BOMBlock) + block.size);
 				store->blockTable[i] = block;
 			}
 		}
